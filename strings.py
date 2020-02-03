5d1ea7b5cb5425f3d7ba7cbdfcb361c3df3e573b
angela <angela@aviak.ca>
2020-02-02 17:05:09 -0700

    section 8

---
+++ strings.py
@@ -0,0 +1,40 @@
+# strings
+x = 'say hi to bob\'s mother'
+# escape characters for '\n - new line \' - quote
+# """ or ''' for multiline
+
+x.islower()
+x.isupper()
+x.lower()
+x.upper()
+x.isalpha()
+x.isalnum() #numbers and letters
+x.isdecimal() #numbers only
+x.isspace() #whitespace only
+x.istitle() #strings are capitalized
+x.startswith('Hello')
+x.endswith('world')
+','.join(['cats', 'rats', 'bats'])
+'\n\n',join(['cats', 'rats', 'bats'])
+'Hello'.rjust(10)
+len('     Hello')
+'Hello'.ljust(10)
+'Hello'.ljust(20)
+'Hello'.center(20, '=') #pad with spaces
+'Hello      '.strip() #remove whitespace
+'Hello      '.rstrip()
+'      Hello   '.lstrip()
+'SpamSpamBaconSpam'.strip('ampS')
+spam.replace('e', 'XYZ')
+
+import pyperclip
+
+pyperclip.copy('Hello!!!!!!!!!!!')
+pyperclip.paste()
+
+#string formatting
+#%s
+name = 'Angela'
+place = 'Bob\'s'
+time = '6pm'
+print('Hello %s, you are invited to a party at %s at %s' % (name, place, time))
